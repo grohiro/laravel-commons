4520db19627a71866109bbd590d5364aebebbbb7
grohiro <khirokawa@growk.jp>
2020-11-16 13:07:13 +0900

    Allow to customize log output

--- config/laravel_commons.php
+++ config/laravel_commons.php
@@ -1,9 +1,22 @@
 <?php
 return [
     'logging' => [
-        'request' => true,
-        'query' => true,
-        'http' => true,
+        'request' => [
+            'enable' => true,
+            'path' => storage_path('logs/request.log'),
+        ],
+        'query' => [
+            'enable' => true,
+            'path' => storage_path('logs/query.log'),
+        ],
+        'http' => [
+            'enable' => true,
+            'path' => storage_path('logs/http.log'),
+        ],
+        'console' => [
+            'enable' => true,
+            'path' => storage_path('logs/console.log'),
+        ],
     ],
     'log_level' => 'debug',
 ];
